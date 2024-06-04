1aa966abbb8f9a6eef60fea56b8ed88024ab1ba4
puresmash <puresmash@gmail.com>
2024-06-05 02:11:22 +0800

    Optimize LC 207 for better performance

--- topological-sorting/207-course-schedule.ts
+++ topological-sorting/207-course-schedule.ts
@@ -9,36 +9,36 @@ import { expect } from 'chai';
  * Topological sorting has two algorithms: Kahn's algorithm and DFS.
  */
 function canFinish(numCourses: number, prerequisites: number[][]): boolean {
-  // [[0, 1], [0, 2], [1, 2]] => [[1, 2], [2], []]
-  // Note that the way to initialize an 2D array in JS is a bit tricky
-  const adjList = prerequisites.reduce<number[][]>((acc, [course, pre]) => {
-    acc[course].push(pre);
-    return acc;
-  }, [...Array(numCourses)].map(() => []));
+  const topology = [];
+  // [[0, 1], [0, 2], [1, 2]] => { 0: [1, 2], 1: [2], 2: [] }
+  const graph: Record<number, number[]> = {};
+  // count the incoming edges for each vertex
   const inDegree = Array(numCourses).fill(0);
   prerequisites.forEach(([course, pre]) => {
+    if (!graph[course]) graph[course] = [];
+    graph[course].push(pre);
     inDegree[pre]++;
   });
-  const result = [];
+  // Kahn's algorithm, start at the vertex with no incoming edges (source vertex)
   const queue = inDegree.reduce<number[]>((acc, degree, index) => {
     if (!degree) acc.push(index);
     return acc;
   }, [])
+  // Start from the source vertex, removing the out-going edges.
+  // After removing, if there is a new vertex with no incoming edges, add it to the queue too.
+  // Keep doing this until the queue is empty.
   while (queue.length) {
     const course = queue.shift()!;
-    result.push(course);
-    // Reduce count by one for every edge in adjList
-    adjList[course].forEach(pre => {
-      inDegree[pre]--;
-      if (inDegree[pre] === 0) {
-        queue.push(pre);
+    topology.push(course);
+    // Reduce count by one for every adjacent vertex
+    (graph[course] ?? []).forEach(neighbor => {
+      inDegree[neighbor]--;
+      if (inDegree[neighbor] === 0) {
+        queue.push(neighbor);
       }
     });
   }
-  if (result.length === numCourses) {
-    return true;
-  }
-  return false;
+  return topology.length === numCourses;
 }
 
 expect(canFinish(2, [[1, 0]])).to.be.true;
