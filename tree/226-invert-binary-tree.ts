59653df79874a0cebf69a43f39cdcb4221be802a
puresmash <puresmash@gmail.com>
2024-06-19 17:20:57 +0800

    Refactor LC 226

--- tree/226-invert-binary-tree.ts
+++ tree/226-invert-binary-tree.ts
@@ -1,17 +1,33 @@
-import { TreeNode, arrayToTree, printTreeBFS } from '../core/Tree.js';
+import { expect } from 'chai';
+import { TreeNode, arrayToTree, treeToArray } from '../core/Tree.js';
 
+/**
+ * 226. Invert Binary Tree
+ * Easy
+ * Tree, DFS
+ * Time complexity: O(n), Space complexity: O(n)
+ * 
+ * Steps:
+ * 1. Iterate the tree in DFS order
+ * 2. For each node, swap the left and right children
+ * 3. Do this until reach the leaf node
+ */
 function invertTree(root: TreeNode | null): TreeNode | null {
-  function dst(node: TreeNode) {
+  function dfs(node: TreeNode) {
     if (!node.left && !node.right) return;
     const left = node.left;
     node.left = node.right;
     node.right = left;
-    if (node.left) dst(node.left);
-    if (node.right) dst(node.right);
+    if (node.left) dfs(node.left);
+    if (node.right) dfs(node.right);
   }
-  if (root) dst(root);
+  if (root) dfs(root);
   return root;
-};
+}
 
-const tree = arrayToTree([4,2,7,1,3,6,9]);
-printTreeBFS(invertTree(tree));
+let tree = arrayToTree([4, 2, 7, 1, 3, 6, 9]);
+expect(treeToArray(invertTree(tree))).to.deep.equal([4, 7, 2, 9, 6, 3, 1]);
+tree = arrayToTree([2, 1, 3]);
+expect(treeToArray(invertTree(tree))).to.deep.equal([2, 3, 1]);
+tree = arrayToTree([1, 2, 3, null, 4, 5]);
+expect(treeToArray(invertTree(tree))).to.deep.equal([1, 3, 2, null, 5, 4]);
